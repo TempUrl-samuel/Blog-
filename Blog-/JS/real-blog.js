64e99700e0e5dfb801532083c023a86ed949b28c
INCON! <samuelcaetanosr79@gmail.com>
2026-07-30 12:53:57 -0300

    Add files via upload

---
+++ Blog-/JS/real-blog.js
@@ -0,0 +1,9 @@
+function chance() {
+    const resultado = Math.floor(Math.random() * 1) + 1;
+    if (resultado == 10) {
+        document.location.replace("forgotten.html")
+    }
+}
+
+
+//  window.alert('This site is WIP, so expect for strange things and unfinished sites around here, OK?')
