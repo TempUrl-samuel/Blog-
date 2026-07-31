26c0f238b6a20169e007d624f8b14ce25451b4f7
INCON! <samuelcaetanosr79@gmail.com>
2026-07-31 08:29:29 -0300

    Delete Blog- directory

--- Blog-/JS/real-blog.js
+++
@@ -1,9 +0,0 @@
-function chance() {
-    const resultado = Math.floor(Math.random() * 1) + 1;
-    if (resultado == 10) {
-        document.location.replace("forgotten.html")
-    }
-}
-
-
-//  window.alert('This site is WIP, so expect for strange things and unfinished sites around here, OK?')
